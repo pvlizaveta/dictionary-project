f871a3a0432ad6ffb112107fc8645c3312308460
pvlizaveta <59615682+pvlizaveta@users.noreply.github.com>
2021-07-22 14:24:51 -0400

    updated some css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
+
 function App() {
   return (
     <div className="App">
       <section>
-      <header className="App-header">What word do you want to look up?</header>
-      <main>
-        <Dictionary defaultKeyWord="dictionary"/>
-      </main>
-      </section> 
-      <footer className="text-center"><a href="https://github.com/pvlizaveta/dictionary-project">Coded</a> by Lizaveta Pauliushchyk</footer>
-      
+        <header className="App-header">
+          What word do you want to look up?
+        </header>
+        <main>
+          <Dictionary defaultKeyWord="dictionary" />
+        </main>
+      </section>
+      <footer className="text-center">
+        <a href="https://github.com/pvlizaveta/dictionary-project">Coded</a> by
+        Lizaveta Pauliushchyk
+      </footer>
     </div>
   );
 }
