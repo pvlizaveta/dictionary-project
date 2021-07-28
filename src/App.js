f81d30a591007b00d112239b262ccddb10bbd062
pvlizaveta <59615682+pvlizaveta@users.noreply.github.com>
2021-07-28 16:32:02 -0400

    improved responsive css

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <section>
-        <header className="App-header">
-          What word do you want to look up?
-        </header>
-        <main>
-          <Dictionary defaultKeyWord="dictionary" />
-        </main>
-      </section>
-      <footer className="text-center">
-        <a href="https://github.com/pvlizaveta/dictionary-project">Coded</a> by
-        Lizaveta Pauliushchyk
-      </footer>
+      <div className="gradient">
+        <div className="pre-section">
+          <header className="header">What word do you want to look up?</header>
+          <main>
+            <Dictionary defaultKeyWord="dictionary" />
+          </main>
+        </div>
+        <footer className="text-center">
+          <a href="https://github.com/pvlizaveta/dictionary-project">Coded</a>{" "}
+          by Lizaveta Pauliushchyk
+        </footer>
+      </div>
     </div>
   );
 }
